3e85555f755dfdbb43133dcbd13e87dad798755d
Paolo Bueno <paolohaji@gmail.com>
2016-09-27 20:53:04 -0300

    bash hates spaces

--- .travis/sonarqube.sh
+++ .travis/sonarqube.sh
@@ -2,9 +2,9 @@
 set -ev
 if [ -v SONAR_TOKEN ]; then
   docker pull pierrevincent/sonar-runner
-  SONAR_OPTIONS = "-Dsonar.login=$SONAR_TOKEN"
+  SONAR_OPTIONS="-Dsonar.login=$SONAR_TOKEN"
   if [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then
-    SONAR_OPTIONS = "$SONAR_OPTIONS -Dsonar.github.oauth=$SONAR_GITHUB -Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST -Dsonar.analysis.mode=issues"
+    SONAR_OPTIONS="$SONAR_OPTIONS -Dsonar.github.oauth=$SONAR_GITHUB -Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST -Dsonar.analysis.mode=issues"
   fi
   docker run --rm -v $(pwd):/data pierrevincent/sonar-runner $SONAR_OPTIONS
 fi
